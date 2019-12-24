f42e5c539b3100fcb805d3198b2ab2e99e63c4a7
Petuuuhhh <peterswanton191@bellsouth.net>
2019-12-23 23:52:42 -0500

    finally

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 var Config = Config || {};
 
-/* version */ Config.version = "0.11.2 (c3aa32d9)";
+/* version */ Config.version = "0.11.2 (90b02725)";
 
 Config.bannedHosts = ['cool.jit.su', 'pokeball-nixonserver.rhcloud.com'];
 
